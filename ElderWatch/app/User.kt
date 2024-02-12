f4f85651c2ab0b72534e5565e5f81c678398ac2b
joc014 <159303235+joc014@users.noreply.github.com>
2024-02-12 09:55:54 -0600

    Add files via upload

---
+++ ElderWatch/app/User.kt
@@ -0,0 +1,13 @@
+val user = User(
+    id = 1,
+    username = "Josh.Cotrau",
+    password = "password123",
+    email = "Josh.Cotrau@example.com",
+    firstName = "Josh",
+    lastName = "Cotrau",
+    address = "123 Main St",
+    city = "Anytown",
+    state = "CA",
+    zipCode = "12345",
+    phoneNumber = "555-123-4567"
+)
